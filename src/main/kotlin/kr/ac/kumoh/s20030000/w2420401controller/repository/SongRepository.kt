a4d100f9edc284758ca735ebebc702c89f54ee54
BWOH <bwoh@outlook.kr>
2024-10-01 22:44:27 +0900

    ♻️ hardcoding 제거

--- src/main/kotlin/kr/ac/kumoh/s20030000/w2420401controller/repository/SongRepository.kt
+++ src/main/kotlin/kr/ac/kumoh/s20030000/w2420401controller/repository/SongRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class SongRepository {
-    val songs = listOf(
+    protected val songs = listOf(
         Song(1, "사랑에 연습이 있었다면", "임재현"),
         Song(2, "테스형", "나훈아"),
         Song(3, "소주 한 잔", "임창정"),
@@ -13,4 +13,6 @@ class SongRepository {
 
     fun fetchSong() = songs
     fun getSong(index: Int) = songs[index]
+    val songsSize: Int
+        get() = songs.size
 }
